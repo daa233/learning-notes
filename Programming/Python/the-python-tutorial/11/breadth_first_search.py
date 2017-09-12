fd48ba0b923dbf82ad3bd56d7af19eac538d088d
Du Ang <du2ang233@gmail.com>
2017-09-12 13:43:01 +0800

    Finish section 11 learning notes of The Python Tutorial in Programming/Python.

---
+++ Programming/Python/the-python-tutorial/11/breadth_first_search.py
@@ -0,0 +1,7 @@
+unsearched = deque([starting_node])
+def breadth_first_search(unsearched):
+    node = unsearched.popleft()
+    for m in gen_moves(node):
+        if is_goal(m):
+            return m
+        unsearched.append(m)
