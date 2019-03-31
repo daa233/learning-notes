c5cf22b2c8be0aa14557b565a978324b8bf60eba
Du Ang <du2ang233@gmail.com>
2019-03-31 21:48:27 +0800

    Update LeetCode 4. Median of Two Sorted Arrays

--- programming/leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ programming/leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -44,7 +44,7 @@ int find_the_kth_largest_num(vector<int>::const_iterator A, int m,
     }
 }
 
-class Solution {
+class Solution1 {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
@@ -57,6 +57,88 @@ public:
             double b = find_the_kth_largest_num(nums1.begin(), m, nums2.begin(), n,
                                          (m + n) / 2 + 1);
             return (a + b) / 2.;
+        }
+    }
+};
+
+double find_the_kth_largest_num_optimize(vector<int>::const_iterator A, int m,
+                                         vector<int>::const_iterator B, int n,
+                                         int k, bool find_next)
+{
+    if (m > n)
+        return find_the_kth_largest_num_optimize(B, n, A, m, k, find_next);
+    if (m == 0) {
+        if (!find_next) {
+            return *(B + k - 1);
+        } else {
+            double a = *(B + k - 1);
+            double b = *(B + k);
+            return (a + b) / 2.;
+        }
+    }
+    if (k == 1) {
+        if (!find_next) {
+            return min(*A, *B);
+        } else {
+            double a, b;
+            if (*A < *B) {
+                a = *A;
+                if (m > 1) {
+                    b = min(*(A+1), *B);
+                } else {
+                    b = *B;
+                }
+            } else if (*A == *B) {
+                return *A;
+            } else {
+                a = *B;
+                if (n > 1) {
+                    b = min(*A, *(B + 1));
+                } else {
+                    b = *A;
+                }
+            }
+            return (a + b) / 2.;
+        }
+    }
+    int i = min(m, k / 2), j = k - i;
+    if (*(A + i - 1) < *(B + j - 1)) {
+        return find_the_kth_largest_num_optimize(A + i, m - i, B, n, k - i,
+                                                 find_next);
+    } else if (*(A + i - 1) > *(B + j - 1)) {
+        return find_the_kth_largest_num_optimize(A, m, B + j, n - j, k - j,
+                                                 find_next);
+    } else {
+        if (!find_next) {
+            return *(A + i - 1);
+        } else {
+            double a = *(A + i - 1);
+            double b;
+            if (i <= m && j <= n) {
+                b = min(*(A + i), *(B + j));
+            } else if (j <= n) {
+                b = *(B + j);
+            } else if (i <= m) {
+                b = *(A + i);
+            } else {
+                exit(-1);
+            }
+            return (a + b) / 2.;
+        }
+    }
+}
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        bool find_next = ((m + n) % 2 == 0);
+        if (find_next) {
+            return find_the_kth_largest_num_optimize(
+                nums1.begin(), m, nums2.begin(), n, (m + n) / 2, find_next);
+        } else {
+            return find_the_kth_largest_num_optimize(
+                nums1.begin(), m, nums2.begin(), n, (m + n) / 2 + 1, find_next);
         }
     }
 };
