541328921e51d26eca377be09d796a5a19489803
DAA233 <du2ang233@gmail.com>
2021-04-08 23:35:24 +0800

    reconstruct the paint app

--- profession/opencv-python-tutorials/gui_features/paint.py
+++ profession/opencv-python-tutorials/gui_features/paint.py
@@ -2,92 +2,109 @@ import cv2
 import numpy as np
 
 
-def get_init_canvas(shape):
-    img = np.ones(shape, dtype=np.uint8) * 255
-    return img
+class PaintApp:
+    """ A simple Paint application. """
+
+    def __init__(self, config):
+        self.config = config
+        self.window_name = self.config.window_name
+        self.canvas = self._get_init_canvas(self.config.canvas_shape)
+        cv2.namedWindow(self.window_name)
+
+        self.color = self.config.default_color
+        self.brush_size = self.config.default_brush_size
+
+        self.drawing = False  # true if the mouse is pressed
+        self._ix, self._iy = -1, -1
+
+        self.set_callbacks()
+
+    def _get_init_canvas(self, shape):
+        img = np.ones(shape, dtype=np.uint8) * 255
+        return img
+
+    def draw(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.drawing = True
+            cv2.circle(self.canvas, (x, y), self.brush_size // 2, self.color, -1)
+            self._ix, self._iy = x, y
+        elif event == cv2.EVENT_MOUSEMOVE:
+            if self.drawing:
+                cv2.line(self.canvas, (self._ix, self._iy), (x, y), self.color, self.brush_size, lineType=cv2.LINE_AA)
+                self._ix, self._iy = x, y
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.drawing = False
+
+    def change_color(self, x):
+        r = cv2.getTrackbarPos("R", self.window_name)
+        g = cv2.getTrackbarPos("G", self.window_name)
+        b = cv2.getTrackbarPos("B", self.window_name)
+        self.color = (b, g, r)
+
+    def change_brush_size(self, x):
+
+        brush_trackbar_value = cv2.getTrackbarPos("Brush Size", self.window_name)
+
+        if brush_trackbar_value <= 0:
+            self.brush_size = 1
+        else:
+            self.brush_size = brush_trackbar_value
+
+    def set_callbacks(self):
+        # set mouse callback to draw
+        cv2.setMouseCallback(self.window_name, self.draw)
+
+        # create trackbars for color change
+        cv2.createTrackbar("R", self.window_name, self.color[2], 255, self.change_color)
+        cv2.createTrackbar("G", self.window_name, self.color[1], 255, self.change_color)
+        cv2.createTrackbar("B", self.window_name, self.color[0], 255, self.change_color)
+
+        # create trackbar for brush radius change
+        cv2.createTrackbar(
+            "Brush Size", self.window_name, self.brush_size, self.config.max_brush_size, self.change_brush_size
+        )
+
+    def release(self):
+        cv2.destroyAllWindows()
+
+    def run(self):
+        while True:
+            cv2.imshow(self.window_name, self.canvas)
+            k = cv2.waitKey(self.config.refresh_frame_interval) & 0xFF
+            if k == ord("r"):
+                # press 'r' to reset the canvas
+                self.canvas = self._get_init_canvas(self.config.canvas_shape)
+            elif k == ord("+"):
+                # press '+' to increase the brush size
+                self.brush_size += 1
+                self.brush_size = min(self.brush_size, self.config.max_brush_size)
+                cv2.setTrackbarPos("Brush Size", self.window_name, self.brush_size)
+            elif k == ord("-"):
+                # press '-' to increase the brush size
+                self.brush_size -= 1
+                self.brush_size = max(self.brush_size, self.config.min_brush_size)
+                cv2.setTrackbarPos("Brush Size", self.window_name, self.brush_size)
+            elif k == 27:
+                # press 'Esc' to quit the demo
+                break
+
+        self.release()
 
 
-canvas_shape = (512, 512, 3)
-img = get_init_canvas(canvas_shape)
-window_name = "paint"
-cv2.namedWindow(window_name)
+class PaintAppConfig:
+    """ The config for the Paint application. """
 
-drawing = False  # true if the mouse is pressed
-mode = True  # if True, draw rectangle. Press 'm' to toggle to curve
-ix, iy = -1, -1
-color = (0, 0, 0)
-brush_size = 5
-min_brush_size = 1
-max_brush_size = 100
-
-
-def draw(event, x, y, flags, param):
-    global img, drawing, mode, ix, iy, color, brush_size
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        cv2.circle(img, (x, y), brush_size // 2, color, -1)
-        ix, iy = x, y
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing:
-            cv2.line(img, (ix, iy), (x, y), color, brush_size, lineType=cv2.LINE_AA)
-            ix, iy = x, y
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
+    def __init__(self):
+        self.window_name = "paint"
+        self.canvas_shape = (512, 512, 3)
+        self.default_color = (0, 0, 0)
+        self.default_brush_size = 5
+        self.min_brush_size = 1
+        self.max_brush_size = 100
+        self.refresh_frame_interval = 1
 
 
-def change_color(x):
-    global color
-
-    r = cv2.getTrackbarPos("R", window_name)
-    g = cv2.getTrackbarPos("G", window_name)
-    b = cv2.getTrackbarPos("B", window_name)
-    color = (b, g, r)
-
-
-def change_brush_size(x):
-    global brush_size
-
-    brush_trackbar_value = cv2.getTrackbarPos("Brush Size", window_name)
-
-    if brush_trackbar_value <= 0:
-        brush_size = 1
-    else:
-        brush_size = brush_trackbar_value
-
-
-# set mouse callback to draw
-cv2.setMouseCallback(window_name, draw)
-
-# create trackbars for color change
-cv2.createTrackbar("R", window_name, color[2], 255, change_color)
-cv2.createTrackbar("G", window_name, color[1], 255, change_color)
-cv2.createTrackbar("B", window_name, color[0], 255, change_color)
-
-# create trackbar for brush radius change
-cv2.createTrackbar("Brush Size", window_name, brush_size, max_brush_size, change_brush_size)
-
-while True:
-    cv2.imshow(window_name, img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == ord("m"):
-        # press 'm' to toggle mode
-        mode = not mode
-    if k == ord("r"):
-        # press 'r' to reset the canvas
-        img = get_init_canvas(canvas_shape)
-    if k == ord("+"):
-        # press '+' to increase the brush size
-        brush_size += 1
-        brush_size = min(brush_size, max_brush_size)
-        cv2.setTrackbarPos("Brush Size", window_name, brush_size)
-    if k == ord("-"):
-        # press '-' to increase the brush size
-        brush_size -= 1
-        brush_size = max(brush_size, min_brush_size)
-        cv2.setTrackbarPos("Brush Size", window_name, brush_size)
-    elif k == 27:
-        # press 'Esc' to quit the demo
-        break
-
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    config = PaintAppConfig()
+    app = PaintApp(config)
+    app.run()
