00743bfcc4972f983f314380739c30ecb1f7b8a2
DAA233 <du2ang233@gmail.com>
2021-04-10 11:20:31 +0800

    support undo in the paint app

--- profession/opencv-python-tutorials/gui_features/paint.py
+++ profession/opencv-python-tutorials/gui_features/paint.py
@@ -16,6 +16,7 @@ class PaintApp:
 
         self.drawing = False  # true if the mouse is pressed
         self._ix, self._iy = -1, -1
+        self.cache = []
 
         self.set_callbacks()
 
@@ -23,8 +24,19 @@ class PaintApp:
         img = np.ones(shape, dtype=np.uint8) * 255
         return img
 
+    def _update_cache(self, canvas):
+        if len(self.cache) >= self.config.max_cache_history:
+            self.cache.pop(0)
+
+        self.cache.append(canvas)
+
+    def undo(self):
+        if len(self.cache) > 0:
+            self.canvas = self.cache.pop(-1)
+
     def draw(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
+            self._update_cache(self.canvas.copy())
             self.drawing = True
             cv2.circle(self.canvas, (x, y), self.brush_size // 2, self.color, -1)
             self._ix, self._iy = x, y
@@ -64,16 +76,25 @@ class PaintApp:
             "Brush Size", self.window_name, self.brush_size, self.config.max_brush_size, self.change_brush_size
         )
 
+    def show(self):
+        cv2.imshow(self.window_name, self.canvas)
+
     def release(self):
         cv2.destroyAllWindows()
 
     def run(self):
         while True:
-            cv2.imshow(self.window_name, self.canvas)
+            self.show()
+
             k = cv2.waitKey(self.config.refresh_frame_interval) & 0xFF
+
             if k == ord("r"):
                 # press 'r' to reset the canvas
+                self._update_cache(self.canvas)
                 self.canvas = self._get_init_canvas(self.config.canvas_shape)
+            elif k == ord("u"):
+                # press 'u' to undo the drawing
+                self.undo()
             elif k == ord("+"):
                 # press '+' to increase the brush size
                 self.brush_size += 1
@@ -102,6 +123,7 @@ class PaintAppConfig:
         self.min_brush_size = 1
         self.max_brush_size = 100
         self.refresh_frame_interval = 1
+        self.max_cache_history = 50
 
 
 if __name__ == "__main__":
