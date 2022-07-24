4d29df352ea019d7a25ae4772f6e3887925c684b
DAA233 <du2ang233@gmail.com>
2022-07-24 12:51:48 +0800

    add tyscheme section 5 lexical varibles - pseudo number - simulate throwing dice

--- programming/tyscheme/5-lexical-variables/pseudorandom-number-generator.scm
+++ programming/tyscheme/5-lexical-variables/pseudorandom-number-generator.scm
@@ -7,3 +7,24 @@
 (random 10)     ; 2
 (random 100)    ; 79
 (random 100)    ; 61
+
+;;; Simulate the throwing of a fair, six-headed die:
+;;; The outcomes 1, 2, 3, 4, 5, 6 are equally likely.
+(define throw-one-die
+    (lambda ()
+        (let ((result (ceiling (* (random 1.0) 6))))
+            result)))
+
+(throw-one-die) ; 2
+(throw-one-die) ; 2
+(throw-one-die) ; 5
+(throw-one-die) ; 3
+
+(define throw-two-dice
+    (lambda ()
+        (+ (throw-one-die) (throw-one-die))))
+
+(throw-two-dice)    ; 12
+(throw-two-dice)    ; 7
+(throw-two-dice)    ; 7
+(throw-two-dice)    ; 11
