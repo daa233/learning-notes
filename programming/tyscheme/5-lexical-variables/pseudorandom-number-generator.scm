6d88ca6357178bfde90a079c5da8fddf0d7d23db
DAA233 <du2ang233@gmail.com>
2022-07-24 12:42:44 +0800

    add tyscheme section 5 lexical varibles - pseudo number

---
+++ programming/tyscheme/5-lexical-variables/pseudorandom-number-generator.scm
@@ -0,0 +1,9 @@
+;;; Pseudorandom-number generators
+;;; http://web.mit.edu/scheme_v9.2/doc/mit-scheme-ref/Random-Numbers.html
+(random 1.0)    ; .5273047071607297
+(random 1.0)    ; .1881378458637763
+(random 1.0)    ; .09894813585508044
+(random 10)     ; 7
+(random 10)     ; 2
+(random 100)    ; 79
+(random 100)    ; 61
