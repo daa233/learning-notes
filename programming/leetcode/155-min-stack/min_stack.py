3d97aa598121e4871e060589dc71ea9edae96f4e
DAA233 <du2ang233@gmail.com>
2023-02-05 14:50:09 +0800

    update leetcode 155-min-stack

--- programming/leetcode/155-min-stack/min_stack.py
+++ programming/leetcode/155-min-stack/min_stack.py
@@ -1,16 +1,16 @@
 # @problem: 155. Min Stack
 # @file: min_stack.py
 # @url: https://leetcode-cn.com/problems/min-stack
-# @description: 
-# 
+# @description:
+#
 # Design a stack that supports push, pop, top, and retrieving the
 # minimum element in constant time.
-# 
+#
 # push(x) -- Push element x onto stack.
 # pop() -- Removes the element on top of the stack.
 # top() -- Get the top element.
 # getMin() -- Retrieve the minimum element in the stack.
-# 
+#
 # Example:
 # MinStack minStack = new MinStack();
 # minStack.push(-2);
@@ -20,12 +20,16 @@
 # minStack.pop();
 # minStack.top();      --> Returns 0.
 # minStack.getMin();   --> Returns -2.
-# 
+#
 # @author: Du Ang
 # @date: Sep 14, 2019
 
-# Retain a min_stack, push/pop the current min element along with stack
+
 class MinStack:
+    """
+    维护一个辅助栈 min_stack，在执行 push、pop 操作时，同时将此时 min_val_stack 栈中对应的最小元素也
+    push 或 pop。
+    """
 
     def __init__(self):
         """
@@ -37,25 +41,22 @@ class MinStack:
     def push(self, x: int) -> None:
         self.stack.append(x)
         if self.min_stack:
-            if x < self.min_stack[-1]:
-                self.min_stack.append(x)
-            else:
-                self.min_stack.append(self.min_stack[-1])
+            min_val = self.getMin()
+            if x < min_val:
+                min_val = x
         else:
-            self.min_stack.append(x)
+            min_val = x
+        self.min_stack.append(min_val)
 
     def pop(self) -> None:
-        if self.stack and self.min_stack:
-            self.min_stack.pop()
-            self.stack.pop()
+        self.stack.pop(-1)
+        self.min_stack.pop(-1)
 
     def top(self) -> int:
-        if self.stack:
-            return self.stack[-1]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        if self.min_stack:
-            return self.min_stack[-1]
+        return self.min_stack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
@@ -64,6 +65,3 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
-
-
-
