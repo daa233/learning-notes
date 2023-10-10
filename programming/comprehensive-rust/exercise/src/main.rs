c26ff7da5ecf8d7e253a229fcd195a382b6988b3
DAA233 <du2ang233@gmail.com>
2023-10-10 23:11:13 +0800

    update comprehensive rust: add structs and methods examples

--- programming/comprehensive-rust/exercise/src/main.rs
+++ programming/comprehensive-rust/exercise/src/main.rs
@@ -1109,6 +1109,129 @@ fn lifetimes() {
     println!("p3: {p3:?}");
 }
 
+// Structs and Methods
+fn structs_and_methods() {
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: u8,
+    }
+
+    let mut peter = Person {
+        name: String::from("Peter"),
+        age: 27,
+    };
+    println!("{} is {} years old", peter.name, peter.age);
+
+    peter.age = 28;
+    println!("{} is {} years old", peter.name, peter.age);
+
+    let jackie = Person {
+        name: String::from("Jackie"),
+        ..peter // copy the majority of the fields from struct 'peter'
+    };
+    println!("{} is {} years old", jackie.name, jackie.age);
+
+    // tuple structs
+    struct Point(i32, i32);
+
+    let p = Point(17, 23);
+    println!("({}, {})", p.0, p.1);
+
+    // tuple structs are often used for single-field wrappers (called newtypes)
+    // - the number is measured in some units: e.g. Newtons
+    // - the value passed some validation when it was created, so you no longer have
+    //   to validate it again.
+    struct Newtons(f64);
+    let force = Newtons(10.0);
+    println!("the force is {} Newtons", force.0);
+
+    impl Person {
+        fn new(name: String, age: u8) -> Self {
+            Self { name, age }
+        }
+
+        // NOTE: use `&self` instead of `self` here to support multiple-times calling
+        // - `&self`: borrows the object from the caller using a shared and
+        //     immutable reference
+        // - `&mut self`: borrows the object from the caller using a unique
+        //     and mutable reference
+        // - `self`: takes ownership of the object and moves it away from the caller
+        // - `mut self`: same as above, but the method can mutate the object.
+        // - No receiver: this becomes a static method on the struct.
+        //     Typically used to create constructors which are called new by convention.
+        fn say_hello(&self) {
+            println!("Hello, my name is {}", self.name);
+        }
+    }
+
+    impl Default for Person {
+        fn default() -> Person {
+            Person {
+                name: "Bot".to_string(),
+                age: 0,
+            }
+        }
+    }
+
+    let tmp = Person::new(String::from("Peter"), 27);
+    println!("{tmp:?}");
+    tmp.say_hello();
+    tmp.say_hello();
+
+    let tmp = Person {
+        name: "Sam".to_string(),
+        ..Person::default()
+    };
+    println!("{tmp:#?}");
+}
+
+// Methods Example
+fn methods_example() {
+    #[derive(Debug)]
+    struct Race {
+        name: String,
+        laps: Vec<i32>,
+    }
+
+    impl Race {
+        fn new(name: &str) -> Race {
+            // No receiver, a static method
+            Race { name: String::from(name), laps: Vec::new() }
+        }
+
+        fn add_lap(&mut self, lap: i32) {
+            // Exclusive borrowed read-write access to self
+            self.laps.push(lap);
+        }
+
+        fn print_laps(&self) {
+            // Shared and read-only borrowed access to self
+            println!("Recorded {} laps for {}:", self.laps.len(), self.name);
+            for (idx, lap) in self.laps.iter().enumerate() {
+                println!("Lap {idx}: {lap} sec");
+            }
+        }
+
+        fn finish(self) {
+            // Exclusive ownership of self
+            let total = self.laps.iter().sum::<i32>();
+            println!("Race {} is finished, total lap time: {}", self.name, total);
+        }
+    }
+
+    let mut race = Race::new("Monaco Grand Prix");
+    race.add_lap(70);
+    race.add_lap(68);
+    race.print_laps();
+    race.add_lap(71);
+    race.print_laps();
+    race.finish();
+
+    // error[E0382]: borrow of moved value: `race`
+    // race.add_lap(42);
+}
+
 fn main() {
     // Program entry point
     println!("\n# Hello World");
@@ -1237,4 +1360,10 @@ fn main() {
 
     println!("\n# Lifetimes");
     lifetimes();
+
+    println!("\n# Structs and Methods");
+    structs_and_methods();
+
+    println!("\n# Methods Example");
+    methods_example();
 }
