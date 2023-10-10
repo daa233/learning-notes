bae8b6f8b5608e9cdd7f43020fa3032d1f352ba9
duangang <duangang@megvii.com>
2023-10-10 13:06:28 +0800

    update comprehensive rust: add exercises Luhn Algorithm and Expression Evaluation

--- programming/comprehensive-rust/exercise/src/main.rs
+++ programming/comprehensive-rust/exercise/src/main.rs
@@ -760,6 +760,221 @@ fn match_guards() {
     }
 }
 
+// Luhn Algorithm
+pub fn luhn(cc_number: &str) -> bool {
+    let mut sum: u32 = 0;
+    let mut count: u32 = 0;
+    for c in cc_number.chars().rev() {
+        if c == ' ' {
+            continue;
+        } else if !c.is_digit(10) {
+            return false;
+        }
+        count += 1;
+        let addend: u32;
+        let digit = c.to_string().parse::<u32>().unwrap();
+        if count % 2 == 0 {
+            let doubled = digit * 2;
+            if doubled > 9 {
+                addend = 1 + (doubled % 10);
+            } else {
+                addend = doubled;
+            }
+        } else {
+            addend = digit;
+        }
+        sum += addend;
+    }
+    count >= 2 && sum % 10 == 0
+}
+
+#[test]
+fn test_non_digit_cc_number() {
+    assert!(!luhn("foo"));
+    assert!(!luhn("foo 0 0"));
+}
+
+#[test]
+fn test_empty_cc_number() {
+    assert!(!luhn(""));
+    assert!(!luhn(" "));
+    assert!(!luhn("  "));
+    assert!(!luhn("    "));
+}
+
+#[test]
+fn test_single_digit_cc_number() {
+    assert!(!luhn("0"));
+}
+
+#[test]
+fn test_two_digit_cc_number() {
+    assert!(luhn(" 0 0 "));
+}
+
+#[test]
+fn test_valid_cc_number() {
+    assert!(luhn("4263 9826 4026 9299"));
+    assert!(luhn("4539 3195 0343 6467"));
+    assert!(luhn("7992 7398 713"));
+}
+
+#[test]
+fn test_invalid_cc_number() {
+    assert!(!luhn("4223 9826 4026 9299"));
+    assert!(!luhn("4539 3195 0343 6476"));
+    assert!(!luhn("8273 1232 7352 0569"));
+}
+
+fn run_luhn_algirithm() {
+    let cc_number_array = ["4263 9826 4026 9299", "4223 9826 4026 9299"];
+    for cc_number in &cc_number_array {
+        let valid_or_not: &str = if luhn(cc_number) { "valid" } else { "invalid" };
+        println!("The credit card number '{cc_number}' is {valid_or_not}");
+    }
+}
+
+/// An operation to perform on two subexpressions.
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+/// An expression, in tree form.
+#[derive(Debug)]
+enum Expression {
+    /// An operation on two subexpressions.
+    Op {
+        op: Operation,
+        // the Box type here is a smart pointer, unboxed it by '*boxed_expr' when use it
+        left: Box<Expression>,
+        right: Box<Expression>,
+    },
+
+    /// A literal value
+    Value(i64),
+}
+
+/// The result of evaluating an expression.
+#[derive(Debug, PartialEq, Eq)]
+enum Res {
+    /// Evaluation was successful, with the given result.
+    Ok(i64),
+    /// Evaluation failed, with the given error message.
+    Err(String),
+}
+// Allow `Ok` and `Err` as shorthands for `Res::Ok` and `Res::Err`.
+use Res::{ Err, Ok };
+
+/// A simple recursive evaluator for arithmetic expressions.
+fn eval(e: Expression) -> Res {
+    match e {
+        // NOTE: using destructing here
+        Expression::Op { op, left, right } => {
+            let left = match eval(*left) {
+                Ok(v) => v,
+                Err(msg) => {
+                    return Err(msg);
+                }
+            };
+            let right = match eval(*right) {
+                Ok(v) => v,
+                Err(msg) => {
+                    return Err(msg);
+                }
+            };
+            let res = match op {
+                Operation::Add => left + right,
+                Operation::Sub => left - right,
+                Operation::Mul => left * right,
+                Operation::Div => {
+                    if right == 0 {
+                        return Err(String::from("division by zero"));
+                    } else {
+                        left / right
+                    }
+                }
+            };
+            Ok(res)
+        }
+        Expression::Value(value) => Ok(value),
+    }
+}
+
+fn run_eval() {
+    let res = eval(Expression::Op {
+        op: Operation::Add,
+        left: Box::new(Expression::Value(10)),
+        right: Box::new(Expression::Value(20)),
+    });
+    match res {
+        Ok(value) => {
+            println!("eval 10 + 20 = {:?}", value);
+        }
+        Err(msg) => {
+            println!("Error: {msg:?}");
+        }
+    }
+}
+
+#[test]
+fn test_value() {
+    assert_eq!(eval(Expression::Value(19)), Ok(19));
+}
+
+#[test]
+fn test_sum() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(10)),
+            right: Box::new(Expression::Value(20)),
+        }),
+        Ok(30)
+    );
+}
+
+#[test]
+fn test_recursion() {
+    let term1 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Value(10)),
+        right: Box::new(Expression::Value(9)),
+    };
+    let term2 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(3)),
+            right: Box::new(Expression::Value(4)),
+        }),
+        right: Box::new(Expression::Value(5)),
+    };
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(term1),
+            right: Box::new(term2),
+        }),
+        Ok(85)
+    );
+}
+
+#[test]
+fn test_error() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Div,
+            left: Box::new(Expression::Value(99)),
+            right: Box::new(Expression::Value(0)),
+        }),
+        Err(String::from("division by zero"))
+    );
+}
+
 fn main() {
     // Program entry point
     println!("\n# Hello World");
@@ -861,4 +1076,10 @@ fn main() {
 
     println!("\n# Match Gurads");
     match_guards();
+
+    println!("\n# Luhn Algorithm");
+    run_luhn_algirithm();
+
+    println!("\n# Expression Evaluation");
+    run_eval();
 }
