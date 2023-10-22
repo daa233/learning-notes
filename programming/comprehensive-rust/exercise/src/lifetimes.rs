e97b5563d3dcbe3d4811402230426b9a00f3a126
daa233 <du2ang233@gmail.com>
2023-10-22 22:43:13 +0800

    update comprehensive rust: update lifetimes (wip)

--- programming/comprehensive-rust/exercise/src/lifetimes.rs
+++ programming/comprehensive-rust/exercise/src/lifetimes.rs
@@ -1,18 +1,109 @@
 // Lifetimes
-pub fn lifetimes() {
-    #[derive(Debug)]
-    struct Point(i32, i32);
 
-    fn left_most<'a>(p1: &'a Point, p2: &'a Point) -> &'a Point {
-        if p1.0 < p2.0 {
-            p1
-        } else {
-            p2
-        }
+// `print_refs` takes two references to `i32` which have different
+// lifetimes `'a` and `'b`. These two lifetimes must both be at
+// least as long as the function `print_refs`.
+fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("x is {} and y is {}", x, y);
+}
+
+// One input reference with lifetime `'a` which must live
+// at least as long as the function.
+fn print_one<'a>(x: &'a i32) {
+    println!("`print_one`: x is {}", x);
+}
+
+// Multiple elements with different lifetimes. In this ase, it
+// would be fine for both to have the same lifetime `'a`, but
+// in more complex cases, different lifetimes may be required.
+fn print_multi<'a, 'b>(x: &'a i32, y: &'b i32) {
+    println!("`print_multi`: x is {}, y is {}", x, y);
+}
+
+// Returning references that have been passed in in acceptable.
+// However, the correct lifetime must be returned.
+fn pass_x<'a, 'b>(x: &'a i32, _: &'b i32) -> &'a i32 {
+    x
+}
+
+// Mutable reference are possible with lifetimes as well
+fn add_one<'a>(x: &'a mut i32) {
+    *x += 1;
+}
+
+// lifetime in funtion
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct Owner(i32);
+
+impl Owner {
+    // Annotate lifetimes as in a standalone function.
+    fn add_one<'a>(&'a mut self) {
+        self.0 += 1;
+    }
+    fn print<'a>(&'a self) {
+        println!("`print`: {}", self.0);
+    }
+}
+
+// A type `Borrowed` which houses a reference to an
+// `i32`. The reference to `i32` must outlive `Borrowed`.
+#[derive(Debug)]
+struct Borrowed<'a>(&'a i32);
+
+// Similarly, both references here must outlive this structure.
+#[derive(Debug)]
+struct NamedBorrowed<'a> {
+    x: &'a i32,
+    y: &'a i32,
+}
+
+// An enum which is either an `i32` or a reference to one.
+#[derive(Debug)]
+enum Either<'a> {
+    Num(i32),
+    Ref(&'a i32),
+}
+
+pub fn lifetimes() {
+    let (four, nine) = (4, 9);
+
+    // Borrows (`&`) of both variables are passed into the function.
+    print_refs(&four, &nine);
+    // Any input which is borrowed must outlive the borrower.
+    // In other words, the lifetime of `four` and `nine` must
+    // be longer than that of `print_refs`.
+
+    // Functions
+    print_one(&four);
+    print_multi(&four, &nine);
+
+    let z = pass_x(&four, &nine);
+    print_one(z);
+
+    let mut t = 5;
+    add_one(&mut t);
+    print_one(&t);
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
     }
 
-    let p1 = Point(10, 10);
-    let p2 = Point(20, 20);
-    let p3: &Point = left_most(&p1, &p2);
-    println!("p3: {p3:?}");
+    // Methods
+    let mut owner = Owner(18);
+    owner.add_one();
+    owner.print();
+
+    // Structs
+    let single = Borrowed(&four);
 }
