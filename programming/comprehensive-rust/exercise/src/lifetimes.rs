ac9ee1048961d652394515848e547e6a74fe45e1
daa233 <du2ang233@gmail.com>
2023-10-25 23:16:08 +0800

    update comprehensive rust: update lifetimes

--- programming/comprehensive-rust/exercise/src/lifetimes.rs
+++ programming/comprehensive-rust/exercise/src/lifetimes.rs
@@ -1,5 +1,7 @@
 // Lifetimes
 
+use std::fmt::Debug;
+
 // `print_refs` takes two references to `i32` which have different
 // lifetimes `'a` and `'b`. These two lifetimes must both be at
 // least as long as the function `print_refs`.
@@ -87,6 +89,55 @@ struct Ref<'a, T: 'a>(&'a T);
 // *references* in `T` must outlive `'a`. Additionally, the lifetime
 // of `Ref` may not exceed `'a`.
 
+// A gneeric function which prints using the `Debug` trait.
+fn print<T>(t: T)
+where
+    T: Debug,
+{
+    println!("`print`: t is {:?}", t);
+}
+
+// Here a reference to `T` is taken where `T` implements
+// `Debug` and all *references* in `T` outlives `'a`. In
+// addition, `'a` must outlive the function.
+fn print_ref<'a, T>(t: &'a T)
+where
+    T: Debug + 'a,
+{
+    println!("`print_ref`: t is {:?}", t);
+}
+
+// Here, Rust infers a lifetime that is as short as possible.
+// The two references are then coerced to that lifetime.
+fn multiply<'a>(first: &'a i32, second: &'a i32) -> i32 {
+    first * second
+}
+
+// `<'a: 'b, 'b>` reads as lifetime `'a` is at least as long as `'b`.
+// Here, we take in an `&'a i32` and return a `&'b i32` as a result of coercion.
+fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+    first
+}
+
+// A reference with 'static lifetime, the data pointed to by the reference lives
+// for the entire lifetime of the running program.
+static HELLO_WORLD: &'static str = "hello world";
+static NUM: i32 = 18;
+
+// 'static as part of a trait bound
+fn generic<T>(x: T)
+where
+    T: 'static + Debug,
+{
+    println!("`x` is {:?}", x);
+}
+
+// Returns a reference to `NUM` where its `'static`
+// lifetime is coerced to that of the input argument.
+fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
+    &NUM
+}
+
 pub fn lifetimes() {
     let (four, nine) = (4, 9);
 
@@ -135,4 +186,40 @@ pub fn lifetimes() {
     // Traits
     let b: Borrowed = Default::default();
     println!("b is {:?}", b);
+
+    // Bounds
+    let ref_x = Ref(&x);
+    print_ref(&ref_x);
+    print(ref_x);
+
+    // Coercion
+    let first = 2; // Longer lifetime
+    {
+        let second = 3; // Shorter lifetime
+        println!("The product is {}", multiply(&first, &second));
+        println!("{} is the first", choose_first(&first, &second));
+    }
+
+    // Static
+    generic(&HELLO_WORLD);
+    {
+        // Make a `string` literal and print it:
+        let static_string = "I'm in read-only memory";
+        println!("static_string: {}", static_string);
+
+        // When `static_string` goes out of scope, the reference
+        // can no longer be used, but the data remains in the binary.
+    }
+    {
+        // Make an integer to use for `coerce_static`:
+        let lifetime_num = 9;
+        // Coerce `NUM` to lifetime of `lifetime_num`:
+        let coerced_static = coerce_static(&lifetime_num);
+
+        println!("coerced_statc: {}", coerced_static);
+        // the value of `coerced_static` is `NUM`'s value. However,
+        // `coerced_static` is not accessible when goes out of the scope.
+    }
+
+    println!("NUM: {} stays accessible!", NUM);
 }
