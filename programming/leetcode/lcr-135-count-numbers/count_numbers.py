adf88fcb9f9c4d72a1bd47f2335f45115b6fbdd2
daa233 <du2ang233@gmail.com>
2023-12-14 23:43:22 +0800

    leetcode: update lcof-17-count-numbers methods

--- programming/leetcode/lcr-135-count-numbers/count_numbers.py
+++ programming/leetcode/lcr-135-count-numbers/count_numbers.py
@@ -1,7 +1,7 @@
 # @problem: LCR 135. 报数
 # @file: count_numbers.py
 # @url: https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof
-# @description: 
+# @description:
 #
 # 实现一个十进制数字报数程序，请按照数字从小到大的顺序返回一个整数数列，该数列从数字 1 开始，到最大的正整数 cnt 位数字结束。
 # 示例 1:
@@ -18,7 +18,77 @@
 
 from typing import List
 
-class Solution:
+
+class Solution1:
     """直接生成，不考虑大数越界"""
+
     def countNumbers(self, cnt: int) -> List[int]:
-        return [i for i in range(1, 10 ** cnt)]
+        return [i for i in range(1, 10**cnt)]
+
+
+class Solution2:
+    """考虑大数越界"""
+
+    def countNumbers(self, cnt: int) -> List[int]:
+        result = []
+
+        def dfs(i: int):
+            if i == cnt:  # 已固定所有数位，终止
+                # 找到第一个不是 0 的数字位
+                non_zero_index = None
+                for index in range(len(nums)):
+                    if nums[index] != "0":
+                        non_zero_index = index
+                        break
+                if non_zero_index is not None:
+                    # 将数字转换为 int 存储，并去掉开头的0
+                    result.append(int("".join(nums[non_zero_index:])))
+                return
+            for j in range(10):
+                nums[i] = str(j)  # 固定第i位为数字j
+                dfs(i + 1)  # 开始固定第i+1位
+
+        nums = ["0"] * cnt
+        dfs(0)
+
+        return result
+
+
+from typing import Optional
+
+
+class Solution:
+    """考虑大数越界，优化去掉开头0的方法"""
+
+    def countNumbers(self, cnt: int) -> List[int]:
+        result = []
+
+        def dfs(i: int, start: Optional[int] = None):
+            if i == cnt:  # 已固定所有数位，终止
+                if start is not None:
+                    # 将数字转换为 int 存储，并去掉开头的0
+                    result.append(int("".join(nums[start:])))
+                return
+
+            for j in range(10):
+                if j != 0:
+                    if start is None:
+                        vi = i  # 还没有最高有效位，当前第i位就是最高有效位
+                    else:
+                        # 沿用原来的最高有效位
+                        vi = start
+                else:
+                    vi = start  # 沿用原来的最高有效位
+                nums[i] = str(j)  # 固定第i位为数字j
+                dfs(i + 1, vi)  # 开始固定第i+1位
+
+        nums = ["0"] * cnt
+        dfs(0)
+
+        return result
+
+
+if __name__ == "__main__":
+    sln = Solution()
+    result = sln.countNumbers(2)
+    print(result)
